DominoTree/freebsd-src

commit 9992eb435829a910a2a25c564bf8d2ad7c6557b0

gif: use hashalloc(9)
Functional change is that on destruction INVARIANTS checks will run.  Also
the mask is no longer hardcoded, so makes it easier to make hash size a
tunable.

Reviewed by:		ae
Differential Revision:	https://reviews.freebsd.org/D56176

sys/netinet/in_gif.c
@@ -46,6 +46,7 @@
 #include <sys/sysctl.h>
 #include <sys/malloc.h>
 #include <sys/proc.h>
+#include <sys/hash.h>
 
 #include <net/ethernet.h>
 #include <net/if.h>
@@ -81,15 +82,17 @@ SYSCTL_INT(_net_inet_ip, IPCTL_GIF_TTL, gifttl, CTLFLAG_VNET | CTLFLAG_RW,
  */
 VNET_DEFINE_STATIC(struct gif_list *, ipv4_hashtbl) = NULL;
 VNET_DEFINE_STATIC(struct gif_list *, ipv4_srchashtbl) = NULL;
+VNET_DEFINE_STATIC(u_int, ipv4_hashmask);
 VNET_DEFINE_STATIC(struct gif_list, ipv4_list) = CK_LIST_HEAD_INITIALIZER();
 #define	V_ipv4_hashtbl		VNET(ipv4_hashtbl)
 #define	V_ipv4_srchashtbl	VNET(ipv4_srchashtbl)
+#define	V_ipv4_hashmask		VNET(ipv4_hashmask)
 #define	V_ipv4_list		VNET(ipv4_list)
 
 #define	GIF_HASH(src, dst)	(V_ipv4_hashtbl[\
-    in_gif_hashval((src), (dst)) & (GIF_HASH_SIZE - 1)])
+    in_gif_hashval((src), (dst)) & (V_ipv4_hashmask - 1)])
 #define	GIF_SRCHASH(src)	(V_ipv4_srchashtbl[\
-    fnv_32_buf(&(src), sizeof(src), FNV1_32_INIT) & (GIF_HASH_SIZE - 1)])
+    fnv_32_buf(&(src), sizeof(src), FNV1_32_INIT) & (V_ipv4_hashmask - 1)])
 #define	GIF_HASH_SC(sc)		GIF_HASH((sc)->gif_iphdr->ip_src.s_addr,\
     (sc)->gif_iphdr->ip_dst.s_addr)
 static uint32_t
@@ -218,8 +221,15 @@ in_gif_ioctl(struct gif_softc *sc, u_long cmd, caddr_t data)
 			break;
 		}
 		if (V_ipv4_hashtbl == NULL) {
-			V_ipv4_hashtbl = gif_hashinit();
-			V_ipv4_srchashtbl = gif_hashinit();
+			struct hashalloc_args ha = {
+				.size = GIF_HASH_SIZE,
+				.mtype = M_GIF,
+				.mflags = M_WAITOK,
+				.head = HASH_HEAD_CK_LIST,
+			};
+			V_ipv4_hashtbl = hashalloc(&ha);
+			V_ipv4_srchashtbl = hashalloc(&ha);
+			V_ipv4_hashmask = ha.size - 1;
 		}
 		error = in_gif_checkdup(sc, src->sin_addr.s_addr,
 		    dst->sin_addr.s_addr);
@@ -438,9 +448,14 @@ in_gif_uninit(void)
 		ip_encap_unregister_srcaddr(ipv4_srcaddrtab);
 	}
 	if (V_ipv4_hashtbl != NULL) {
-		gif_hashdestroy(V_ipv4_hashtbl);
+		struct hashalloc_args ha = {
+			.size = V_ipv4_hashmask + 1,
+			.mtype = M_GIF,
+			.head = HASH_HEAD_CK_LIST,
+		};
+		hashfree(V_ipv4_hashtbl, &ha);
 		V_ipv4_hashtbl = NULL;
 		GIF_WAIT();
-		gif_hashdestroy(V_ipv4_srchashtbl);
+		hashfree(V_ipv4_srchashtbl, &ha);
 	}
 }

sys/netinet6/in6_gif.c
@@ -47,6 +47,7 @@
 #include <sys/sysctl.h>
 #include <sys/malloc.h>
 #include <sys/proc.h>
+#include <sys/hash.h>
 
 #include <net/ethernet.h>
 #include <net/if.h>
@@ -86,15 +87,17 @@ SYSCTL_INT(_net_inet6_ip6, IPV6CTL_GIF_HLIM, gifhlim,
  */
 VNET_DEFINE_STATIC(struct gif_list *, ipv6_hashtbl) = NULL;
 VNET_DEFINE_STATIC(struct gif_list *, ipv6_srchashtbl) = NULL;
+VNET_DEFINE_STATIC(u_int, ipv6_hashmask);
 VNET_DEFINE_STATIC(struct gif_list, ipv6_list) = CK_LIST_HEAD_INITIALIZER();
 #define	V_ipv6_hashtbl		VNET(ipv6_hashtbl)
 #define	V_ipv6_srchashtbl	VNET(ipv6_srchashtbl)
+#define	V_ipv6_hashmask		VNET(ipv6_hashmask)
 #define	V_ipv6_list		VNET(ipv6_list)
 
 #define	GIF_HASH(src, dst)	(V_ipv6_hashtbl[\
-    in6_gif_hashval((src), (dst)) & (GIF_HASH_SIZE - 1)])
+    in6_gif_hashval((src), (dst)) & (V_ipv6_hashmask - 1)])
 #define	GIF_SRCHASH(src)	(V_ipv6_srchashtbl[\
-    fnv_32_buf((src), sizeof(*src), FNV1_32_INIT) & (GIF_HASH_SIZE - 1)])
+    fnv_32_buf((src), sizeof(*src), FNV1_32_INIT) & (V_ipv6_hashmask - 1)])
 #define	GIF_HASH_SC(sc)		GIF_HASH(&(sc)->gif_ip6hdr->ip6_src,\
     &(sc)->gif_ip6hdr->ip6_dst)
 static uint32_t
@@ -237,8 +240,15 @@ in6_gif_ioctl(struct gif_softc *sc, u_long cmd, caddr_t data)
 			break;
 
 		if (V_ipv6_hashtbl == NULL) {
-			V_ipv6_hashtbl = gif_hashinit();
-			V_ipv6_srchashtbl = gif_hashinit();
+			struct hashalloc_args ha = {
+				.size = GIF_HASH_SIZE,
+				.mtype = M_GIF,
+				.mflags = M_WAITOK,
+				.head = HASH_HEAD_CK_LIST,
+			};
+			V_ipv6_hashtbl = hashalloc(&ha);
+			V_ipv6_srchashtbl = hashalloc(&ha);
+			V_ipv6_hashmask = ha.size - 1;
 		}
 		error = in6_gif_checkdup(sc, &src->sin6_addr,
 		    &dst->sin6_addr);
@@ -469,9 +479,14 @@ in6_gif_uninit(void)
 		ip6_encap_unregister_srcaddr(ipv6_srcaddrtab);
 	}
 	if (V_ipv6_hashtbl != NULL) {
-		gif_hashdestroy(V_ipv6_hashtbl);
+		struct hashalloc_args ha = {
+			.size = V_ipv6_hashmask + 1,
+			.mtype = M_GIF,
+			.head = HASH_HEAD_CK_LIST,
+		};
+		hashfree(V_ipv6_hashtbl, &ha);
 		V_ipv6_hashtbl = NULL;
 		GIF_WAIT();
-		gif_hashdestroy(V_ipv6_srchashtbl);
+		hashfree(V_ipv6_srchashtbl, &ha);
 	}
 }

sys/net/if_gif.c
@@ -254,27 +254,6 @@ static moduledata_t gif_mod = {
 DECLARE_MODULE(if_gif, gif_mod, SI_SUB_PSEUDO, SI_ORDER_ANY);
 MODULE_VERSION(if_gif, 1);
 
-struct gif_list *
-gif_hashinit(void)
-{
-	struct gif_list *hash;
-	int i;
-
-	hash = malloc(sizeof(struct gif_list) * GIF_HASH_SIZE,
-	    M_GIF, M_WAITOK);
-	for (i = 0; i < GIF_HASH_SIZE; i++)
-		CK_LIST_INIT(&hash[i]);
-
-	return (hash);
-}
-
-void
-gif_hashdestroy(struct gif_list *hash)
-{
-
-	free(hash, M_GIF);
-}
-
 #define	MTAG_GIF	1080679712
 static int
 gif_transmit(struct ifnet *ifp, struct mbuf *m)

sys/net/if_gif.h
@@ -96,10 +96,6 @@ struct etherip_header {
 
 #define	GIF_WAIT()	epoch_wait_preempt(net_epoch_preempt)
 
-/* Prototypes */
-struct gif_list *gif_hashinit(void);
-void gif_hashdestroy(struct gif_list *);
-
 void gif_input(struct mbuf *, struct ifnet *, int, uint8_t);
 int gif_output(struct ifnet *, struct mbuf *, const struct sockaddr *,
 	       struct route *);

commit a35a51eb93ac0332ad9892518bf58219fe2e3fb4

gif: stop masking off half of the hash tables
GIF_HASH() and GIF_SRCHASH() index with

	& (V_ipv4_hashmask - 1)

but V_ipv4_hashmask is already the table size minus one, assigned as

	V_ipv4_hashmask = ha.size - 1;

so the extra subtraction makes the mask size - 2.  GIF_HASH_SIZE is
1 << 4, and hashalloc(9) rounds that to 16 and allocates 16 entries,
giving a mask of 15.  The index expression is therefore h & 14, which
forces bit 0 of the bucket number to zero: buckets 1, 3, 5, 7, 9, 11,
13 and 15 are unreachable for the life of the VNET.  Half of each
table is dead, for every possible hash value, not just some corner
case.

The same error is present four times: GIF_HASH() and GIF_SRCHASH() in
both in_gif.c and in6_gif.c.

Insert and lookup both go through these macros and so agree, and the
tables are consulted per received tunnelled datagram by in_gif_lookup()
and in6_gif_lookup().  Nothing is misrouted; the cost is that a host
with many gif(4) tunnels walks roughly twice the chain it should on the
receive path, on a table that only has 16 buckets to begin with.

This is a regression from 9992eb435829 ("gif: use hashalloc(9)"), which
replaced the correct

	in_gif_hashval((src), (dst)) & (GIF_HASH_SIZE - 1)

with the hashmask form while the mask was being made dynamic, and
subtracted one twice.  Drop the extra subtraction: V_ipv4_hashmask is
15, so & V_ipv4_hashmask is exactly the & (GIF_HASH_SIZE - 1) the code
had before, and spans all 16 buckets again.

Signed-off-by: Nick Price <nick@spun.io>

sys/netinet/in_gif.c
@@ -90,9 +90,9 @@ VNET_DEFINE_STATIC(struct gif_list, ipv4_list) = CK_LIST_HEAD_INITIALIZER();
 #define	V_ipv4_list		VNET(ipv4_list)
 
 #define	GIF_HASH(src, dst)	(V_ipv4_hashtbl[\
-    in_gif_hashval((src), (dst)) & (V_ipv4_hashmask - 1)])
+    in_gif_hashval((src), (dst)) & V_ipv4_hashmask])
 #define	GIF_SRCHASH(src)	(V_ipv4_srchashtbl[\
-    fnv_32_buf(&(src), sizeof(src), FNV1_32_INIT) & (V_ipv4_hashmask - 1)])
+    fnv_32_buf(&(src), sizeof(src), FNV1_32_INIT) & V_ipv4_hashmask])
 #define	GIF_HASH_SC(sc)		GIF_HASH((sc)->gif_iphdr->ip_src.s_addr,\
     (sc)->gif_iphdr->ip_dst.s_addr)
 static uint32_t

sys/netinet6/in6_gif.c
@@ -95,9 +95,9 @@ VNET_DEFINE_STATIC(struct gif_list, ipv6_list) = CK_LIST_HEAD_INITIALIZER();
 #define	V_ipv6_list		VNET(ipv6_list)
 
 #define	GIF_HASH(src, dst)	(V_ipv6_hashtbl[\
-    in6_gif_hashval((src), (dst)) & (V_ipv6_hashmask - 1)])
+    in6_gif_hashval((src), (dst)) & V_ipv6_hashmask])
 #define	GIF_SRCHASH(src)	(V_ipv6_srchashtbl[\
-    fnv_32_buf((src), sizeof(*src), FNV1_32_INIT) & (V_ipv6_hashmask - 1)])
+    fnv_32_buf((src), sizeof(*src), FNV1_32_INIT) & V_ipv6_hashmask])
 #define	GIF_HASH_SC(sc)		GIF_HASH(&(sc)->gif_ip6hdr->ip6_src,\
     &(sc)->gif_ip6hdr->ip6_dst)
 static uint32_t